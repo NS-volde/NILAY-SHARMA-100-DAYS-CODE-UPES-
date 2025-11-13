5b587f936fbac87f27741af285f9bbc82dce31dd
Nilay <grad.nilay@gmail.com>
2025-11-13 23:15:29 +0530

    Add files via upload

---
+++ day22q2.c
@@ -0,0 +1,38 @@
+//Q44: Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+Approximate sum: 3.3
+
+Input 2:
+5
+Output 2:
+Approximate sum: 4.4
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    float sum = 1.0;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    int numerator = 3;
+    int denominator = 4;
+
+    for (int i = 2; i <= n; i++) {
+        sum += (float)numerator / denominator;
+        numerator += 2;
+        denominator += 2;
+    }
+
+    printf("Sum of the series = %.2f\n", sum);
+
+    return 0;
+}
