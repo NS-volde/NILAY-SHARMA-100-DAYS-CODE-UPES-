e7fea6cc9ebc1961ece38d405f8979c6718ed88a
Nilay <grad.nilay@gmail.com>
2025-11-18 12:52:05 +0530

    Add files via upload

---
+++ day37q2.c
@@ -0,0 +1,47 @@
+//Q74: Find the transpose of a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+1 4
+2 5
+3 6
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    scanf("%d %d", &rows, &cols);
+
+    int mat[rows][cols];
+    int trans[cols][rows];
+
+    
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            trans[j][i] = mat[i][j];
+        }
+    }
+
+  
+    for(int i = 0; i < cols; i++) {
+        for(int j = 0; j < rows; j++) {
+            printf("%d ", trans[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
