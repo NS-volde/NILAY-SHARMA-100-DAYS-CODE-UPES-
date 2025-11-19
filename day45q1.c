a8b501ff0fe040ce88f8a7b339827bd4319ae530
Nilay <grad.nilay@gmail.com>
2025-11-19 09:50:46 +0530

    Add files via upload

---
+++ day45q1.c
@@ -0,0 +1,32 @@
+//Q89: Count frequency of a given character in a string.
+
+/*
+Sample Test Cases:
+Input 1:
+programming
+g
+Output 1:
+2
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    char ch;
+    int count = 0;
+
+    scanf("%s", str);   
+    scanf(" %c", &ch);  
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ch) {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
