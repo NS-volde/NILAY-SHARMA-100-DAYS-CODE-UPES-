6c705936a422c7f081bca387947a6e7c3c8c4413
Nilay <grad.nilay@gmail.com>
2025-11-19 09:48:30 +0530

    Add files via upload

---
+++ day42q1.c
@@ -0,0 +1,40 @@
+//Q83: Count vowels and consonants in a string.
+
+/*
+Sample Test Cases:
+Input 1:
+hello
+Output 1:
+Vowels=2, Consonants=3
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int vowels = 0, consonants = 0;
+
+    
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+
+        
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+
+            
+            if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+                ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+    }
+
+    printf("Vowels=%d, Consonants=%d", vowels, consonants);
+
+    return 0;
+}
